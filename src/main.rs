78364d54c6abbaa06c5484515b88f61dd08652f6
hcoffey1 <hcoffey1@vols.utk.edu>
2022-05-15 00:22:45 -0500

    Use gcc to assemble x86

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,16 @@ use std::fs::File;
 use std::io::BufReader;
 use std::io::{Error, ErrorKind};
 use std::path::Path;
+use std::process::Command;
 
 use duck_compiler::{parse, x86_64_gen};
+
+fn get_file_name(path: &str) -> &str {
+    let pos_end = path.find(".ddg").unwrap();
+    let pos_begin = path.rfind("/").unwrap() + 1;
+
+    &path[pos_begin..pos_end]
+}
 
 fn main() -> Result<(), Error> {
     //Get input file path
@@ -18,6 +26,9 @@ fn main() -> Result<(), Error> {
 
     //Open file and init reader
     let path = Path::new(&args[1]);
+
+    let file_name_base = get_file_name(&args[1]);
+
     let file = match File::open(&path) {
         Err(why) => panic!("Couldn't open {}: {}", path.display(), why),
         Ok(file) => file,
@@ -38,7 +49,16 @@ fn main() -> Result<(), Error> {
         }
     }
 
-    x86_64_gen::lower_program(&parse_results, "out.s")?;
+    let file_name_asm = format!("{}.s", file_name_base);
+    x86_64_gen::lower_program(&parse_results, &file_name_asm)?;
+
+    //Kinda cheating and using gcc to assemble for now
+    //Also, this is probably a security vulnerability
+    let output_arg = format!("-o{}", file_name_base);
+    Command::new("gcc")
+        .args([file_name_asm, output_arg])
+        .spawn()
+        .expect("Failed to assemble program.");
 
     Ok(())
 }
